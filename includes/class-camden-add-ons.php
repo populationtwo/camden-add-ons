ffb38dd0d8d9188a64bccbc607a75cc7d9961e81
Edwin <populationtwo@gmail.com>
2017-08-07 11:38:04 -0400

    Bump up version.

--- includes/class-camden-add-ons.php
+++ includes/class-camden-add-ons.php
@@ -69,7 +69,7 @@ class Camden_Add_Ons {
 	public function __construct() {
 
 		$this->plugin_name = 'camden-add-ons';
-		$this->version     = '1.0.0';
+		$this->version     = '1.1.0';
 
 		$this->load_dependencies();
 		$this->set_locale();
